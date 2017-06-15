89a86c3305a10721e95973ab0c67e0f540c7c638
Uche Ugwoaba <ucheugw@gmail.com>
2017-06-14 22:41:26 -0400

    UU starting all over with angular4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,43 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  // template: 'mainTemplate.html'
+  templateUrl: 'app/mainTemplate.html',
+  styleUrls: ['app/styles.css'],
+
+  providers: [HeroService]
 })
-export class AppComponent  { name = 'Angular'; }
+
+
+
+
+export class AppComponent implements OnInit {
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  title = 'Tour of Heroes';
+  selectedHero: Hero;
+  heroes: Hero[];
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    console.log('Hero selected');
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => {
+      this.heroes = heroes;
+      console.log('Promise fufilled');
+    });
+  }
+}
+
+
+
+
